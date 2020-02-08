6c51e13559c0f9876b97c453612d9779412c5e3e
Daman Bhola <42509298+damnbhola@users.noreply.github.com>
2020-02-08 11:38:03 +0530

    second pattern

---
+++ 2.py
@@ -0,0 +1,16 @@
+"""
+    *
+   **
+  ***
+ ****
+*****
+"""
+
+n = int(input())
+for i in range(n):
+    for j in range(n):
+        if i+j >= n-1:
+            print("*", end="")
+        else:
+            print(end=" ")
+    print()
